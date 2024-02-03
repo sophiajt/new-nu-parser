404e4872ae086f6ecacec6b2f17ccdb443f74c6a
Jakub Žádník <kubouch@gmail.com>
2024-02-03 14:22:29 +0200

    Add errors to typechecker

--- src/typechecker.rs
+++ src/typechecker.rs
@@ -1,4 +1,5 @@
 use crate::compiler::Compiler;
+use crate::errors::{Severity, SourceError};
 use crate::parser::NodeId;
 
 #[derive(Debug, Clone, Copy)]
@@ -8,13 +9,17 @@ pub const UNKNOWN_TYPE: TypeId = TypeId(usize::MAX);
 
 pub struct Types {
     pub node_types: Vec<TypeId>,
+    pub errors: Vec<SourceError>,
 }
 
 pub struct Typechecker<'a> {
     // Immutable reference to a compiler after the name binding pass
     compiler: &'a Compiler,
 
-    node_types: Vec<TypeId>,
+    /// Types of nodes. Each type in this vector matches a node in compiler.ast_nodes at the same position.
+    pub node_types: Vec<TypeId>,
+    /// Errors encountered during type checking
+    pub errors: Vec<SourceError>,
 }
 
 impl<'a> Typechecker<'a> {
@@ -22,12 +27,14 @@ impl<'a> Typechecker<'a> {
         Self {
             compiler,
             node_types: vec![UNKNOWN_TYPE; compiler.ast_nodes.len()],
+            errors: vec![],
         }
     }
 
     pub fn to_types(self) -> Types {
         Types {
             node_types: self.node_types,
+            errors: self.errors,
         }
     }
 
@@ -54,7 +61,15 @@ impl<'a> Typechecker<'a> {
 
     pub fn typecheck_node(&mut self, node_id: NodeId) {
         match self.compiler.ast_nodes[node_id.0] {
-            _ => (),
+            _ => self.error("unsupported ast node in typechecker", node_id),
         }
+    }
+
+    pub fn error(&mut self, msg: impl Into<String>, node_id: NodeId) {
+        self.errors.push(SourceError {
+            message: msg.into(),
+            node_id,
+            severity: Severity::Error,
+        })
     }
 }
